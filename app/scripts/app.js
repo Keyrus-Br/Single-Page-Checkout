7c6f220a0f8f10b1ca2ac26ea63967c430d84efa
charles.vatin <charles.vatin@gmail.com>
2015-07-23 18:27:08 -0300

    Integration of the HTML structure of the designer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,15 +21,10 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+	  .when('/', {
+        templateUrl: 'views/checkout.html',
+        controller: 'CheckoutCtrl',
+        controllerAs: 'checkout'
       })
       .otherwise({
         redirectTo: '/'
---
+++ app/scripts/controllers/checkout.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name singlePageCheckoutApp.controller:CheckoutCtrl
+ * @description
+ * # CheckoutCtrl
+ * Controller of the singlePageCheckoutApp
+ */
+angular.module('singlePageCheckoutApp')
+  .controller('CheckoutCtrl', function () {
+    
+  });
